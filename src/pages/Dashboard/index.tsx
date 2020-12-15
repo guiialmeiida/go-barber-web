0991258bd66488838d96aa5a148408f73823a973
guiialmeiida <gui.almeidacrz@gmail.com>
2020-12-15 06:54:03 -0300

    Página de perfil

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,6 +6,7 @@ import 'react-day-picker/lib/style.css';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import { parseISO } from 'date-fns/esm';
+import { Link } from 'react-router-dom';
 import {
   Container,
   Header,
@@ -146,7 +147,9 @@ const Dashboard: React.FC = () => {
 
             <div>
               <span>Bem Bindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
